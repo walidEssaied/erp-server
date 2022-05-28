02550c7a389d48d94415ebba388ecf114e096b6c
BoldDigitalAgency <contact@bold.tn>
2022-05-29 06:28:15 +1300

    fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,5 @@
 const parse = require('pg-connection-string').parse;
-const config = parse("postgres://vfrbollnogqogp:0b3bc6f0e6e5230b2cb6f2e3b681a3ab1b198ea3c4ab6d73562dbcee0848ab22@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/dbm7uo13rbrk1u");
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
