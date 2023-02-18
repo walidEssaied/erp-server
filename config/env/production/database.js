cf75b5d56a6d56ea1febf00d7e95ebcb0d2f2eea
wessaied <walidessaied01@gmail.com>
2023-02-18 13:04:41 +0100

    update config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,15 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+      },
+      options: {
+        useNullAsDefault: true,
       },
     },
-    debug: false,
   },
 });
